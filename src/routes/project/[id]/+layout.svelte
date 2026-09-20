8aec83d9a01ecf67023c284be511d9ab4b909c2d
Erik Sorto <rcksorto@yahoo.com>
2026-09-20 00:00:00 -0700

    Contain "quick filters" as a component

--- src/routes/project/[id]/+layout.svelte
+++ src/routes/project/[id]/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Icon from '$lib/components/Icon.svelte';
 	import Nav from './Nav.svelte';
+	import QuickFilters from './QuickFilters.svelte';
 
 	const { children } = $props();
 
@@ -106,16 +107,7 @@
 	{/if}
 	<div class="flex grow overflow-auto">
 		<Nav />
-		<ul class="overflow-auto">
-			<li><button class="p-2"><Icon class="h-6 w-6" name="ArrowRight" /></button></li>
-			<li><button class="p-2"><Icon class="h-6 w-6" name="Zap" /></button></li>
-			<li><button class="p-2"><Icon class="h-6 w-6" name="StickyNote" /></button></li>
-			<li><button class="p-2"><Icon class="h-6 w-6" name="Mail" /></button></li>
-			<li><button class="p-2"><Icon class="h-6 w-6" name="Printer" /></button></li>
-			<li><button class="p-2"><Icon class="h-6 w-6" name="MessagesSquare" /></button></li>
-			<li><button class="p-2"><Icon class="h-6 w-6" name="CircleCheck" /></button></li>
-			<li><button class="p-2"><Icon class="h-6 w-6" name="Bell" /></button></li>
-		</ul>
+		<QuickFilters />
 		<div class="grow overflow-auto">
 			{@render children()}
 		</div>
---
+++ src/routes/project/[id]/QuickFilters.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Icon from '$lib/components/Icon.svelte';
+</script>
+
+<ul class="overflow-auto">
+	<li><button class="p-2"><Icon class="h-6 w-6" name="ArrowRight" /></button></li>
+	<li><button class="p-2"><Icon class="h-6 w-6" name="Zap" /></button></li>
+	<li><button class="p-2"><Icon class="h-6 w-6" name="StickyNote" /></button></li>
+	<li><button class="p-2"><Icon class="h-6 w-6" name="Mail" /></button></li>
+	<li><button class="p-2"><Icon class="h-6 w-6" name="Printer" /></button></li>
+	<li><button class="p-2"><Icon class="h-6 w-6" name="MessagesSquare" /></button></li>
+	<li><button class="p-2"><Icon class="h-6 w-6" name="CircleCheck" /></button></li>
+	<li><button class="p-2"><Icon class="h-6 w-6" name="Bell" /></button></li>
+</ul>
